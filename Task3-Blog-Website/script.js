e05975befe700d2e2fe0d0ef98d24e653e87e386
SrishtiSarkar-02 <wings22007@gmail.com>
2026-08-15 16:07:43 +0530

    Delete Task3-Blog-Website/script.js

--- Task3-Blog-Website/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".article a").forEach(link => {
-    link.addEventListener("click", function(event) {
-        event.preventDefault();
-        alert("Thanks for reading this article!");
-    });
-});
